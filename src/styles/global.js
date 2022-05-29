c544374827606b4b7822b128a7e5d3cb46f16e29
Thálice Lopes <thlopes2002@gmail.com>
2022-05-29 01:35:38 -0300

    Início da Criação do Sistema e introdução a comandos utilizados

---
+++ src/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+    body{
+        margin: 0;
+        padding: 0;
+        font-family: "poppins", Sans-Serif;
+        background-color: #f2f2f2;
+    }
+`;
+
+export default Global
