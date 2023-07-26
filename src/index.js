743421f527b8490df9a62fd8fec14c2dc507a7b7
renan anthony <renanalmassa17@gmail.com>
2023-07-25 23:53:11 -0300

    testando pull request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.querySelector("#root"));
 
-root.render(<App />)
+
+const root = createRoot(document.getElementById("#root"));
+
+root.render(<h1>Hello, world</h1>)
